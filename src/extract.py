16ea18be5505e1c159675ef94568b51d35db5df5
Biocyborge <138586264+Biocyborge@users.noreply.github.com>
2026-04-14 14:10:48 -0400

    Extract and transform
    modified extract to corrrectly grab data from excel files and save them as a dictionary of data frames and started to work on the transform.py

--- src/extract.py
+++ src/extract.py
@@ -1,13 +1,15 @@
 # This file extracts the datasets using pandas
+# made by Ralu Ofoche
 import pandas as pd
 from pathlib import Path
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # relative directory using os
-BASE_DIR_2 = Path(__file__).resolve.parent.parent #relative directory using path
+BASE_DIR_2 = Path(__file__).resolve().parent.parent #relative directory using path
 
 def data_loader(file_name):
-    file_path = os.path.join(BASE_DIR, file_name)
+    file_path = BASE_DIR_2 / "data" / "raw" / file_name
+    print(f"this is the path {file_path}")
 
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"{file_name} not found.")
@@ -17,17 +19,21 @@ def data_loader(file_name):
     return df
 
 def extracter():
-    customers = data_loader("olist_customers_dataset.csv")
-    geolocation = data_loader("olist_geolocation_dataset.csv")
-    items = data_loader("olist_order_items_dataset")
-    payments = data_loader("olist_order_payment_dataset")
-    reviews = data_loader("olist_order_reviews_dataset")
-    orders = data_loader("olist_orders_dataset")
-    prodcuts = data_loader("olist_products_dataset")
-    sellers = data_loader("olist_sellers_dataset")
-    category = data_loader("product_category_name_translation")
-
-    return customers, geolocation, items, payments, reviews, orders, prodcuts, sellers, category
+    customer_data = {
+        "customers" : data_loader("olist_customers_dataset.csv"),
+        "geolocation" : data_loader("olist_geolocation_dataset.csv"),
+        "items" : data_loader("olist_order_items_dataset.csv"),
+        "payments" : data_loader("olist_order_payments_dataset.csv"),
+        "reviews" : data_loader("olist_order_reviews_dataset.csv"),
+        "orders" : data_loader("olist_orders_dataset.csv"),
+        "prodcuts" : data_loader("olist_products_dataset.csv"),
+        "sellers" : data_loader("olist_sellers_dataset.csv"),
+        "category" : data_loader("product_category_name_translation.csv")
+    }
+    print(customer_data["category"])
+    return customer_data
 
 if __name__ == "__main__":
     extracter()
+    
+
---
+++ src/transform.py
@@ -0,0 +1,9 @@
+#This file transofrms the data
+import pandas as pd
+
+#drop missing order
+def transform(customers,geolocation,items,payments,reviews,orders,prodcuts,sellers,category):
+
+
+
+    return transformed_data
